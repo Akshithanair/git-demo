482617c3016ac47673ba89a88f80e03b7e54939a
Akshithanair <Akshithanair>
2024-03-31 13:27:24 +0530

    added a txt and js file

---
+++ addition.js
@@ -0,0 +1,10 @@
+let akshitha=true;
+let fancy=true;
+if(akshitha!=fancy)
+{
+    console.log("Akshitha is a very high maintainence girl");
+}
+else{
+    console.log("she is a very easy going lady");
+}
+console.log(akshitha==fancy);
